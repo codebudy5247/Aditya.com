ad821d7e1c79cfda6fa39cf9cc3ffd8822d43b8b
aditya <aditya.shekhar@hyperrealitytech.com>
2022-09-11 18:24:20 +0530

    ::hammer

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,10 +1,3 @@
-import meter1 from "../../Assets/Image/meter1.svg";
-import meter2 from "../../Assets/Image/meter2.svg";
-import meter3 from "../../Assets/Image/meter3.svg";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import arrow1 from "../../Assets/Image/arrow1.svg";
-import arrow2 from "../../Assets/Image/arrow2.svg";
 import colorSharp from "../../Assets/Image/color-sharp.png";
 import SkillsTab from "./SkillsTab";
 const Skills = () => {
